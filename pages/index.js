cc76441745dfa98491c5692e986fb0a4051cd412
Kamal_nayan <geniusamansingh@gmail.com>
2022-02-28 19:59:40 +0530

    starting with tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-red-600">Hello World this is kamal </h1>
-      <h1 className="text-red-900">Hello World this is kamal </h1>
+      <h1 className="text-red-600 ">Hello World this is kamal </h1>
+      <h1 className="text-red-500" styles={"font-family: 'Roboto',sans-serif"}>Hello ths is kamal </h1>
+      <div className="text-lg p-4 border-red-500">her</div>    
+      <button className="p-5 m-3 bg-red-300 font-serif hover:bg-green-400">button</button>
     </div>
   );
 }
