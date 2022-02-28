7af3e1591d383eb23c2ecd2cc52b11014cbadc25
Kamal_nayan <geniusamansingh@gmail.com>
2022-02-28 08:10:55 +0530

    initial files

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-green-500">Hello World</h1>
+      <h1 className="text-red-600">Hello World this is kamal </h1>
+      <h1 className="text-red-900">Hello World this is kamal </h1>
     </div>
   );
 }
